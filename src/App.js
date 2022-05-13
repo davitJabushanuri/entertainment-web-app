91be6d4f7520eeadc4dfdcc370c2f7644ac73195
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-13 05:02:40 +0400

    create and style navbar for mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Navbar from './components/Navbar'
+import './scss/main.scss'
+
 function App() {
-	return <div className='App'>movies</div>
+	return (
+		<div className='App'>
+			<Navbar />
+		</div>
+	)
 }
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import Logo from '../assets/logo.svg'
+import HomeIcon from '../assets/icon-nav-home.svg'
+import MoviesIcon from '../assets/icon-nav-movies.svg'
+import TVseriesIcon from '../assets/icon-nav-tv-series.svg'
+import BookmarksIcon from '../assets/icon-nav-bookmark.svg'
+import UserIcon from '../assets/image-avatar.png'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<nav className='navbar'>
+			<div className='navbar__logo'>
+				<img src={Logo} alt='' />
+			</div>
+			<ul className='navbar__links'>
+				<li className='navbar__links__link'>
+					<img src={HomeIcon} alt='' />
+				</li>
+				<li className='navbar__links__link'>
+					<img src={MoviesIcon} alt='' />
+				</li>
+				<li className='navbar__links__link'>
+					<img src={TVseriesIcon} alt='' />
+				</li>
+				<li className='navbar__links__link'>
+					<img src={BookmarksIcon} alt='' />
+				</li>
+			</ul>
+
+			<div className='navbar__user'>
+				<img src={UserIcon} alt='' />
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
