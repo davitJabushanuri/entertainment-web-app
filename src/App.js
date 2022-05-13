aa5fed407f2124f4b641e01768a35f2bd2b5ff68
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-13 18:16:13 +0400

    ge data from data.json and save it in usestate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from 'react'
+
 import Navbar from './components/Navbar'
+import Trending from './components/Trending'
 import './scss/main.scss'
 
+import Database from './data.json'
+
 function App() {
+	const [shows, setShows] = useState(Database)
+	const [movies, setMovies] = useState([])
+	const [tvShows, setTvShows] = useState([])
+
+	const getMovies = Database => {
+		const moviesData = Database.filter(show => show.category === 'Movie')
+		const tvShowsData = Database.filter(show => show.category === 'TV Series')
+		setMovies(moviesData)
+		setTvShows(tvShowsData)
+	}
+
+	useEffect(() => {
+		getMovies(Database)
+	}, [])
+
 	return (
 		<div className='App'>
 			<Navbar />
+			<Trending />
+			{tvShows.map(show => {
+				return <p>{show.title}</p>
+			})}
 		</div>
 	)
 }
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Trending = () => {
-	return <div>Trending</div>
+	return (
+		<section className='trending'>
+			<h2 className='trending__header'>Trending</h2>
+		</section>
+	)
 }
 
 export default Trending
